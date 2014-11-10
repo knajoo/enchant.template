9708e33f3b094ef3110777a228fb048bc8e25d65
knajoo <e14007@std.it-college.ac.jp>
2014-11-10 21:57:54 +0900

    14-11-10

--- main.js
+++ main.js
@@ -6,15 +6,62 @@ var game;
 var gs = {
 		height:320
 		,width:320
-		,fps:30
+		,fps:16
 }; 
+
+var Bear = Class.create(Sprite,{
+	initialize:function(){
+		Sprite.call(this,32,32);
+			this.image = game.assets['http://enchantjs.com/assets/images/chara1.gif'];
+			this.x = (game.width - this.width) / 2;
+			this.y = game.height - this.height - 16;
+			this.status = STATUS_WAIT;
+			this.frame = [10, 11, 10, 12];
+			game.rootScene.addChild(this);
+	},
+		onenterframe:function(){
+			if (game.input.right){
+				this.scaleX = 1;
+						this.x += 5;
+				if (this.x + this.width > game.width){
+						this.x = game.width - this.width;
+				}
+
+			}
+			if (game.input.left){
+				this.scaleX = -1;
+				this.x -= 5;
+			if (this.x + this.width == 0){
+					this.x = game.width - this.width;
+			}
+			}
+		}
+});
+
+var STATUS_WAIT = 0;
+var STATUS_WALK = 1;
+var STATUS_JUMP = 2;
 
 window.onload = function(){
 		game = new Core(gs.width,gs.height);
-/*上記のgs.width,gs.heightの部分を３２０等のように数値で記入すると
-何を指定しているかわからないので、上記のように変数を宣言して記述する。*/
 		game.fps = gs.fps;
-		game.onload = function(){};
+		//画像の読み込み
+		game.preload('http://enchantjs.com/assets/images/chara1.gif',
+									'http://enchantjs.com/assets/images/map0.gif');
+		game.onload = function(){
+	//クマの生成
+	new Bear();
+	/*
+	var bear = new Sprite(32, 32);
+	bear.image = game.assets['http://enchantjs.com/assets/images/chara1.gif'];
+	bear.x = 160 -16;
+	bear.y = 320 - 16 - 32;
+	bear.status = STATUS_WAIT;
+	bear.anim = [10, 11, 10, 12];
+	bear.frame = 10;
+	game.rootScene.addChild(bear);
+	*/	};
 		game.start();
 
 };
+
