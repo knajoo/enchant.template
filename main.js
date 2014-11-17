c93af2d7939cb81d8dd0f2dae185b6e5782de0e0
knajoo <e14007@std.it-college.ac.jp>
2014-11-17 22:05:08 +0900

    first

--- main.js
+++ main.js
@@ -9,12 +9,70 @@ var gs = {
 		,fps:30
 }; 
 
-window.onload = function(){
-		game = new Core(gs.width,gs.height);
-/*上記のgs.width,gs.heightの部分を３２０等のように数値で記入すると
-何を指定しているかわからないので、上記のように変数を宣言して記述する。*/
-		game.fps = gs.fps;
-		game.onload = function(){};
-		game.start();
+var Pit = Class.create(Sprite,{
+	initialize:function(x,y){
+		Sprite.call(this,48,48);
+		this.image = game.assets['./assets/mogura.png'];
+		this.x = x;
+		this.y = y;
+		this.mode = 2;
+		this.nextMode = 0;
+		this.waitFor = game.frame + rand(100);
+		console.log(game.frame + "|" + this.waitFor);
+	},
+	move:function(){
+	switch(this.mode){
+		case 0:
+		this.frame++;
+			if (this.frame > 4 - 1){
+				this.changeMode(1,100);
+		}
+			break;
+		case 1:
+		this.frame--;
+			if (this.frame < 1){
+				this.changeMode(0,100);
+		}
+			break;
+		case 2:
+			if (game.frame > this.waitFor){
+				this.mode = this.nextMode;
+			}
+		}
+	},
+	changeMode:function(nextmode,random){
+			this.mode = 2;
+			this.nextMode = nextmode;
+			this.waitFor = game.frame + rand(random);
+	},
+	onenterframe:function(){
+		this.move();
+	},
+	ontouchstart:function(){
+		this.hit();
+	},
+	hit:function(){
+	  if (this.frame === 5){
+	  		return;
+	  }
+	  if (this.frame > 2 -1){
+		this.frame = 5;
+		this.changeMode(1,10);
+	}
+	}
+});
 
+window.onload = function(){
+	game = new Core(gs.width,gs.height);
+	game.fps = gs.fps;
+	game.preload('./assets/mogura.png');
+	game.onload = function(){
+		var pit = new Pit(100,100);
+		game.rootScene.addChild(pit);
+	};
+	game.start();
 };
+
+function rand(num){
+	return ~~(Math.random() * num);
+}
